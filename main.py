8d5c3a704c923fdeb1cd9fe88619007ab9dc4585
Alejandra.D <alejandra.davila.tipan@udla.edu.ec>
2024-05-05 20:18:00 -0500

    Cambios Finales

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import heapq
 import itertools
 import math
 
-def euclidean_distance(x1, y1, x2, y2):
+def distanciaEuclidiana(x1, y1, x2, y2):
     return math.sqrt(pow((x2 - x1),2) + pow((y2 - y1),2))
 
-def dijkstra(graph, start):
-    distances = {node: float('infinity') for node in graph}
-    distances[start] = 0
-    priority_queue = [(0, start)]
+def dijkstra(puntosPlano, empezar):
+    distances = {node: float('infinity') for node in puntosPlano}
+    distances[empezar] = 0
+    priority_queue = [(0, empezar)]
 
     while priority_queue:
         current_distance, current_node = heapq.heappop(priority_queue)
-        for neighbor, weight in graph[current_node].items():
+        for neighbor, weight in puntosPlano[current_node].items():
             distance = current_distance + weight
             if distance < distances[neighbor]:
                 distances[neighbor] = distance
@@ -20,7 +20,7 @@ def dijkstra(graph, start):
 
     return distances
 
-def find_best_path(graph, start, end):
+def encontrarCamino(graph, start, end):
     nodes = list(graph.keys())
     nodes.remove(start)
     nodes.remove(end)
@@ -41,7 +41,7 @@ def find_best_path(graph, start, end):
     return min_cost, best_path
 
 # Coordenadas de los puntos
-coordinates = {
+coordenadas = {
     'P1': (1, 1),
     'P2': (2, 6),
     'P3': (4, 7),
@@ -50,14 +50,14 @@ coordinates = {
 }
 
 # Crear el grafo con las distancias Euclidianas
-graph = {point: {} for point in coordinates}
-edges = [('P1', 'P2'), ('P1', 'P3'), ('P1', 'P4'), ('P1', 'P5'), ('P2', 'P1'), ('P2', 'P3'), ('P2', 'P4'), ('P2', 'P5'), ('P3', 'P1'), ('P3', 'P2'), ('P3', 'P4'), ('P3', 'P5'), ('P4', 'P1'), ('P4', 'P2'), ('P4', 'P3'),('P4', 'P5'), ('P5', 'P1'), ('P5', 'P2'), ('P5', 'P3'),('P5', 'P4')]
+puntosPlano = {point: {} for point in coordenadas}
+nodos = [('P1', 'P2'), ('P1', 'P3'), ('P1', 'P4'), ('P1', 'P5'), ('P2', 'P1'), ('P2', 'P3'), ('P2', 'P4'), ('P2', 'P5'), ('P3', 'P1'), ('P3', 'P2'), ('P3', 'P4'), ('P3', 'P5'), ('P4', 'P1'), ('P4', 'P2'), ('P4', 'P3'),('P4', 'P5'), ('P5', 'P1'), ('P5', 'P2'), ('P5', 'P3'),('P5', 'P4')]
 
-for (p1, p2) in edges:
-    distance = euclidean_distance(*coordinates[p1], *coordinates[p2])
-    graph[p1][p2] = distance
-    graph[p2][p1] = distance
+for (p1, p2) in nodos:
+    distancia = distanciaEuclidiana(*coordenadas[p1], *coordenadas[p2])
+    puntosPlano[p1][p2] = distancia
+    puntosPlano[p2][p1] = distancia
 
 # Calcula el mejor camino desde P1 a P5
-cost, path = find_best_path(graph, 'P2', 'P1')
+cost, path = encontrarCamino(puntosPlano, 'P2', 'P1')
 print(f"El camino más corto es {path} con una distancia de {cost:.2f} cm")
